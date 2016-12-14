c7d717e531813ea6f50b8a6afb0987d11ac1ff58
Sven <sven.hornberg@gmail.com>
2016-12-14 19:30:46 +0100

    show

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        "semi": 2,
+        "no-undef": 0,
+        "no-undefined": 0,
+        "no-unused-vars": 0,
+        "prefer-template" : 0,
+        "no-console": 0,
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,93 @@
-$( document ).ready(function() {
+function update() {
+  const sexVal = Cookies.get('sex');
+  const sizeVal = Cookies.get('size');
 
+  switch (sexVal) {
+
+    case 'm':
+      $('#outfitmale').removeClass('is-hidden');
+      $('#outfitfemale').addClass('is-hidden');
+      switch (sizeVal) {
+        case 's':
+          $('#mainpic').attr('src', '/img/figuren_s_m.jpg');
+          break;
+        case 'm':
+          $('#mainpic').attr('src', '/img/figuren_m_m.jpg');
+          break;
+        case 'l':
+          $('#mainpic').attr('src', '/img/figuren_l_m.jpg');
+          break;
+
+        default:
+          $('#mainpic').attr('src', '/img/figuren_m_m.png');
+          break;
+      }
+      break;
+
+    case 'f':
+      $('#outfitmale').addClass('is-hidden');
+      $('#outfitfemale').removeClass('is-hidden');
+      switch (sizeVal) {
+        case 's':
+          $('#mainpic').attr('src', '/img/figuren_s.jpg');
+          break;
+        case 'm':
+          $('#mainpic').attr('src', '/img/figuren_m.jpg');
+          break;
+        case 'l':
+          $('#mainpic').attr('src', '/img/figuren_l.jpg');
+          break;
+
+        default:
+          $('#mainpic').attr('src', '/img/figuren_m.jpg');
+          break;
+      }
+      break;
+
+    default:
+      $('#mainpic').attr('src', '/img/mann_m.png');
+      break;
+  }
+  Cookies.set('outfit', 'off');
+}
+
+$(document).ready(() => {
+  update();
+  $('#slider').slider();
+  setTimeout(() => {
+    $('#thankslogo').attr('src', '/img/witt_thanks.png');
+  }, 1200);
+  $('.drag').draggable();
+  $('.drag').click(() => {
+    $('.drag').css('height', '250px');
+    $('.drag').css('width', '250px');
+    $('.drag').find('img').css('height', '250px');
+    $('.drag').find('img').css('width', '250px');
+  });
 });
 
 function outfit(val) {
-  $("#mainpic").attr("src", "/img/outfit" + val +".jpg");
+  $('#mainpic').attr('src', '/img/outfit' + val + '.jpg');
+  Cookies.set('outfit', 'on');
+}
+
+function outfitm(val) {
+  $('#mainpic').attr('src', '/img/outfit' + val + '_m.jpg');
+  Cookies.set('outfit', 'on');
+}
+
+function male() {
+  Cookies.set('sex', 'm');
+  update();
+}
+
+function female() {
+  Cookies.set('sex', 'f');
+  update();
+}
+
+function size(val) {
+  Cookies.set('size', val);
+  console.log(val);
+  update();
 }
